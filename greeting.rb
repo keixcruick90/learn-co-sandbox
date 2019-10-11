3be84620b0c9cc3cdb0c529da0a7f12a1af15f5f
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-11 21:14:19 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,15 @@
+name = "Maria"
+
+def greeting_a_person(name, language)
+  puts "Hello #{name}. We heard you are a great #{language} programmer"
+end
+
+greeting_a_person("Maria", "Ruby")
+
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
+
+stylish_chef
